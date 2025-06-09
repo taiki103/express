4cca965b26abb33df0cecf21e69301f549964d31
taiki103 <taiki.iwasa0117@gmail.com>
2025-06-09 16:03:39 +0900

    Add mongodb access

--- routes/notes.js
+++ routes/notes.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "test";
+const uri = "dami";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
